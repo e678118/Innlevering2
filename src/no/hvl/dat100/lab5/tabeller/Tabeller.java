9eacc953fd691021f865dd814f5035521d1cdfd3
Elise Kristine Andersen <142396134+e678118@users.noreply.github.com>
2023-10-04 16:07:08 +0200

    Update Tabeller.java

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -1,59 +1,68 @@
 package no.hvl.dat100.lab5.tabeller;
 
-public class Tabeller {
+public class Tabeller 
+{
 
 	// a)
-		public static void skrivUt(int[] tabell) { 
-		
+		public static void skrivUt(int[] tabell) 
+	{ 
 			for(int i=0; i<tabell.length; i++)
 				System.out.println(tabell[i]);
 				
-				}
+	}
 
 		// b)
-		public static String tilStreng(int[] tabell) {
-			
+		public static String tilStreng(int[] tabell) 
+	{
 			String resultat = ""; 
 
-			for (int i = 0; i < tabell.length; i++) {
+			for (int i = 0; i < tabell.length; i++) 
+			{
 			    resultat += tabell[i];
-			    if (i < tabell.length - 1) {
+			    if (i < tabell.length - 1) 
+			    {
 			        resultat += ", ";
 			    }
 			}
 
 			return resultat; 
 					   
-		    }
+	}
 			
 
 	// c)
-	public static int summer(int[] tabell) {
-
+	public static int summer(int[] tabell) 
+	{
 		int sum=0;
 		for(int i=0; i<tabell.length; i++)  
 			
 			sum+=tabell[i];
 	
 		return sum;
-		}
+	}
 
 	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
+	public static boolean finnesTall(int[] tabell, int tall) 
+	{
 
-		for(int i=0; i<tabell.length;i++) {
-			if(tabell[i]==tall) { 
+		for(int i=0; i<tabell.length;i++) 
+		{
+			if(tabell[i]==tall) 
+			{ 
 				return true;
-		}
+			}
 		}
 			return false;
 	}
 
 	// e)
-	public static int posisjonTall(int[] tabell, int tall) {
+	public static int posisjonTall(int[] tabell, int tall) 
+	{
 		
-		for(int i=0; i<tabell.length;i++) {
-			if(tabell[i]==tall) {
+		for(int i=0; i<tabell.length;i++) 
+		{
+			if(tabell[i]==tall) 
+			{
 				return i;
 			}
 		}
@@ -61,39 +70,45 @@ public class Tabeller {
 	}
 
 	// f)
-	public static int[] reverser(int[] tabell) {
-			int []nytabell=new int[tabell.length];
+	public static int[] reverser(int[] tabell) 
+	{
+		int []nytabell=new int[tabell.length];
 			
-			for(int i=tabell.length-1; i>=0;i--) 
-				{
-				nytabell[(tabell.length-1)-i]=tabell[i];
-				}
-			return nytabell;
-			}
+		for(int i=tabell.length-1; i>=0;i--) 
+		{
+			nytabell[(tabell.length-1)-i]=tabell[i];
+		}
+		return nytabell;
+	}
 
 	// g)
-	public static boolean erSortert(int[] tabell) {
-
-		for(int i=1;i<tabell.length;i++) {
-			if(tabell[i]<tabell[i-1]) {
+	public static boolean erSortert(int[] tabell) 
+	{
+		for(int i=1;i<tabell.length;i++) 
+		{
+			if(tabell[i]<tabell[i-1]) 
+			{
 			
 			return false;
 			}
-			}
-			return true;
-			}
+		}
+		return true;
+	}
 
 	// h)
-	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+	public static int[] settSammen(int[] tabell1, int[] tabell2) 
+	{
 
 		int nyLengde=tabell1.length + tabell2.length;
 		int []sammen= new int [nyLengde];
-		for(int i=0;i<tabell1.length;i++) {
+		for(int i=0;i<tabell1.length;i++) 
+		{
 			sammen[i]=tabell1[i];
 		}
-		for(int j=0; j<tabell2.length;j++) {
+		for(int j=0; j<tabell2.length;j++) 
+		{
 			sammen[tabell1.length+j]=tabell2[j];
 		}
 		return sammen;
-}
+	}
 }
